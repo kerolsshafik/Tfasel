7b534f00284fc6da9e248babf37784e892daceec
kerolsshafik <kerols.shafik6@gmail.com>
2024-09-30 21:48:10 +0300

    test

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Category extends Model
 {
     use HasFactory;
+    use InteractsWithMedia ;
 
     protected $fillable = ['name'];
     protected $table = 'categories';
